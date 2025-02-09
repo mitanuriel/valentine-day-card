86a7095cccdbc1917a32a1f449fe42e0db27eeca
Elina K. <145558996+mitanuriel@users.noreply.github.com>
2025-02-09 15:43:53 +0100

    added gif to the index page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,6 +4,11 @@ import { ValentineCard } from "@/components/valentine/ValentineCard";
 const Index = () => {
   return (
     <main className="min-h-screen flex items-center justify-center bg-[#FFDEE2] p-4">
+       <img 
+          src="https://tenor.com/bVkhJ.gif" 
+          alt="Valentine GIF" 
+          className="w-64 h-64 object-cover mx-auto mb-4"
+        />
       <ValentineCard />
     </main>
   );
